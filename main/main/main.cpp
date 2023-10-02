3446b6f0c38ee47f8e66b5ef4e0f616f42e1fdc9
도경 <lee7198@naver.com>
2023-10-02 14:22:21 +0900

    print ascii, binary

--- main/main/main.cpp
+++ main/main/main.cpp
@@ -2,48 +2,90 @@
 #include <string>
 using namespace std;
 
-void getASCII(int arr[][8], string plainText) {
-    int num[8];
+struct DES_interface {
+    string plaintext;
+    string ascii;
+};
 
-    cout << "convert ASCII";
-    for(int i=0; i<8; i++) {
-        num[i] = plainText[i];
+class DES {
+    int num[8];
+    int arr[8][8];
+    
+public:
+    DES_interface text;
+    DES() {
+        while(1) {
+            cout << "8자리 이하의 문자를 입력해주세요 : ";
+            cin >> text.plaintext;
+            if(text.plaintext.length() <= 8)
+                break;
+            
+            cout << "8자리를 초과합니다. 다시 입력해주세요." << endl;
+            continue;
+        }
+        for(int i=0; i<8; i++)
+            num[i] = text.plaintext[i];
     }
     
-    // print ASCII
-    cout << "Plaintext (ASC) : ";
-    for(int i=0; i<8; i++) {
-        printf("%d", num[i]);
-    }
-
-    for(int i = 0; i < 8; i++) {
-        int bit = 128;
-        while(bit != 1) {
-            arr[i][0] = (num[i] / bit) ? 1 : 0;
-            num[i] %= bit;
+    void ASCII() {
+        string result;
+        for(int i=0; i<8; i++)
+            num[i] = text.plaintext[i];
+        
+        // print ASCII
+        cout << "Plaintext (ASCII) : ";
+        for(int i=0; i<8; i++) {
+            text.ascii += to_string((int)(num[i]));
+            cout << (int) num[i];
         }
+        printf("\n");
     }
-}
-
-void des() {
-    string plaintext;
-    int arr[8][8];
-
-    while(1) {
-        cout << "8자리 이하의 문자를 입력해주세요 : ";
-        cin >> plaintext;
-        if(plaintext.length() <= 8) {
-            cout << "OK";
-            break;
+    
+    string Binary() {
+        string result;
+        int bin[64], p = 0;
+        
+        for(int i = 0; i < 8; i++) {
+            int bit = 128, j = 0;
+            while(bit > 0) {
+                arr[i][j++] = (num[i] / bit) ? 1 : 0;
+                if(bit != 1) num[i] %= bit;
+                bit /= 2;
+            }
         }
-        cout << "8자리를 초과합니다. 다시 입력해주세요." << endl;
-        continue;
+        for(int i=0; i<8; i++) {
+            for(int j=0; j<8; j++) {
+                bin[j + p] = arr[i][j];
+            }
+            p += 8;
+        }
+        // print Binary
+        cout << "Plaintext (Binary) : ";
+        for(int i=0; i<64; i++) {
+            printf("%d", bin[i]);
+            if((i+1) % 8 == 0)
+                printf(" ");
+        }
+        return "";
     }
-    getASCII(arr, plaintext);
-}
+};
+
 
 int main() {
-    des();
+    DES des;
+    des.ASCII();
+    des.Binary();
+    
     
     return 0;
 }
+
+//void Print_Hex(int w, int Arr[]) {
+//int i, j, num; int k = 0; int a[4];
+//for (i = 0; i < w / 4; i++) {
+//for (j = 0; j < 4; j++) {
+//a[j] = Arr[j + k];
+//num = (a[0] * 8) + (a[1] * 4) + (a[2] * 2) + (a[3] * 1);
+//} printf("%X", num); k = k + 4;
+//} printf("\n");
+//}
