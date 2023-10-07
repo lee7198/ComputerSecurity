b693773feba6f615651a01293551fb8d3dbd1e8d
도경 <lee7198@naver.com>
2023-10-07 14:45:54 +0900

    add Hex

--- main/main/main.cpp
+++ main/main/main.cpp
@@ -3,8 +3,10 @@
 using namespace std;
 
 struct DES_interface {
-    string plaintext;
+    string origin;
     string ascii;
+    string bin;
+    string hex;
 };
 
 class DES {
@@ -12,36 +14,35 @@ class DES {
     int arr[8][8];
     
 public:
-    DES_interface text;
+    DES_interface plainText;
     DES() {
         while(1) {
             cout << "8자리 이하의 문자를 입력해주세요 : ";
-            cin >> text.plaintext;
-            if(text.plaintext.length() <= 8)
+            cin >> plainText.origin;
+            if(plainText.origin.length() <= 8)
                 break;
             
             cout << "8자리를 초과합니다. 다시 입력해주세요." << endl;
             continue;
         }
         for(int i=0; i<8; i++)
-            num[i] = text.plaintext[i];
+            num[i] = plainText.origin[i];
     }
     
     void ASCII() {
         string result;
         for(int i=0; i<8; i++)
-            num[i] = text.plaintext[i];
+            num[i] = plainText.origin[i];
         
-        // print ASCII
-        cout << "Plaintext (ASCII) : ";
+        cout << "Plaintext (ASCII) \t: \t";
         for(int i=0; i<8; i++) {
-            text.ascii += to_string((int)(num[i]));
+            plainText.ascii += to_string((int)(num[i]));
             cout << (int) num[i];
         }
         printf("\n");
     }
     
-    string Binary() {
+    void Binary() {
         string result;
         int bin[64], p = 0;
         
@@ -53,20 +54,36 @@ public:
                 bit /= 2;
             }
         }
-        for(int i=0; i<8; i++) {
-            for(int j=0; j<8; j++) {
+        for(int i=0; i<8; i++)
+            for(int j=0; j<8; j++)
                 bin[j + p] = arr[i][j];
-            }
             p += 8;
-        }
-        // print Binary
-        cout << "Plaintext (Binary) : ";
+        
+        cout << "Plaintext (Binary) \t: \t";
         for(int i=0; i<64; i++) {
             printf("%d", bin[i]);
+            plainText.bin += bin[i];
             if((i+1) % 8 == 0)
                 printf(" ");
         }
-        return "";
+        printf("\n");
+    }
+    
+    void Hex() {
+        int a[4];
+        int k = 0;
+        int num = 0;
+        printf("Plaintext (Hex) \t: \t");
+        for(int i=0; i< 16; i++) {
+            for(int j=0; j<4; j++) {
+                a[j] = plainText.bin[j + k];
+                num = (a[0] * 8) + (a[1] * 4) + (a[2] * 2) + (a[3] * 1);
+            }
+            plainText.hex += num;
+            printf("%X", num);
+            k = k + 4;
+        }
+        printf("\n");
     }
 };
 
@@ -75,17 +92,8 @@ int main() {
     DES des;
     des.ASCII();
     des.Binary();
+    des.Hex();
     
     
     return 0;
 }
-
-//void Print_Hex(int w, int Arr[]) {
-//int i, j, num; int k = 0; int a[4];
-//for (i = 0; i < w / 4; i++) {
-//for (j = 0; j < 4; j++) {
-//a[j] = Arr[j + k];
-//num = (a[0] * 8) + (a[1] * 4) + (a[2] * 2) + (a[3] * 1);
-//} printf("%X", num); k = k + 4;
-//} printf("\n");
-//}
